f87a3d6fcd3e4686ff7c93b17f55ead5faa5a107
lhq <2321812781@qq.com>
2026-06-30 12:29:14 +0800

    HelloWorld4

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld2");
+        System.out.println("HelloWorld4");
     }
 }
